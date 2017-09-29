15bbe4efde0734fba2e0f26cc5892de3a0e32875
Amanda Yuan <amandayuan@berkeley.edu>
2017-09-28 17:27:08 -0700

    done

--- app/controllers/concerns/Post.rb
+++ app/controllers/concerns/Post.rb
@@ -2,7 +2,7 @@ class Post
   attr_accessor :title, :content
 
   # The params hash parameter comes from the form and is passed into this method.
-  # 
+  #
   def initialize(params={})
     @title = params[:title]
     @content = params[:content]
